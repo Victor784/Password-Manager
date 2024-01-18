d35ef183a801b6ae9a064592d932e8fccb40b139
Victor784 <victorpop839@yahoo.com>
2024-01-18 10:21:31 +0200

    PM-42 - bugfix

--- ApiReturnTypes/Password.cs
+++ ApiReturnTypes/Password.cs
@@ -4,18 +4,18 @@ namespace PasswordManagerClient.ApiReturnTypes
 {
     public class Password
     {
-        public string id { get; set; }
-        public string  user_id { get; set; }
+        public int id { get; set; }
+        public int  user_id { get; set; }
         public string associated_email { get; set; }
         public string associated_website { get; set; }
         public string password_value { get; set; }
-        public long time_of_creation { get; set; }
-        public long time_of_last_update { get; set; }
-        public long? expiration_date { get; set; }
+        public string time_of_creation { get; set; }
+        public string time_of_last_update { get; set; }
+        public string? expiration_date { get; set; }
 
         public Password() { }
 
-        public Password(string id, string user_id, string associated_website , string associated_email, string password_value, int time_of_creation, int time_of_last_update, int expiration_date = 0) 
+        public Password(int id, int user_id, string associated_website , string associated_email, string password_value, string time_of_creation, string time_of_last_update, string expiration_date = "") 
         {
             this.id = id;
             this.user_id = user_id;  
@@ -27,9 +27,9 @@ namespace PasswordManagerClient.ApiReturnTypes
             this.expiration_date = expiration_date;
         }
 
-        public Password(string user_id, string associated_website , string associated_email, string password_value, int time_of_creation, int time_of_last_update, int expiration_date = 0)
+        public Password(int user_id, string associated_website , string associated_email, string password_value, string time_of_creation, string time_of_last_update, string expiration_date = "")
         {
-            this.id = "0";
+            this.id = 0;
             this.user_id = user_id;
             this.associated_website = associated_website;
             this.associated_email = associated_email;
