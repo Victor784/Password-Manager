622b22031385a8df07ee638a74efecda4953b7ab
Victor784 <victorpop839@yahoo.com>
2024-01-24 17:34:55 +0200

    PM-23 : Add button functionality added and tested.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PasswordManagerClient;
+using PasswordManagerClient.ApiReturnTypes;
 using PasswordManagerClient.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
