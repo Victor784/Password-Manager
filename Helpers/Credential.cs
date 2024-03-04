5f5dd1c9aef5843d3ee7dd56e49a43fb96f065b7
Victor784 <victorpop839@yahoo.com>
2024-03-04 20:00:58 +0200

    PM-36: Client side user input validation

--- Helpers/Credential.cs
+++ Helpers/Credential.cs
@@ -1,10 +1,16 @@
-﻿namespace PasswordManagerClient.Helpers
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManagerClient.Helpers
 {
     public class Credential
     {
-    
-      public string Email { get; set; } = ""; 
-      public string Password { get; set; } = ""; 
+
+        [Required(ErrorMessage = "Email is required")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Too many / few characters")]
+        public string Email { get; set; } = "";
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; } = ""; 
     }
 
 }
